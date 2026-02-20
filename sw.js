3b2592ac14ffe7ca14b939f2818f2f11a79d3f2d
smirknof <iganoof@gmail.com>
2026-02-20 23:09:31 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'ram-alerta-v15';
+const assets = ['./', './index.html', './logo.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
